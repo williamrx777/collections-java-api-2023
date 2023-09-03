369317ab05876e05f719e645a0bcb1a0927cbd0a
william rodrigues <williamr723@gmail.com>
2023-09-02 21:48:17 -0300

    Resolvendo desafio ListaTarefa Adicionando ordenação

---
+++ src/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,43 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+
+    private List<Pessoa> pessoasList;
+
+    public OrdenacaoPessoa() {
+        this.pessoasList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoasList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoasList);
+        Collections.sort(pessoaPorIdade);
+        return pessoaPorIdade;
+    }
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoasList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.pessoasList);
+
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+    }
+
+}
